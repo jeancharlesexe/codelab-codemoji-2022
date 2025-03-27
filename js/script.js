871a5c6c02953532fc343140a0b209ee8ad4d9de
jeancharlesx64 <jean.albuquerque@sptech.school>
2025-03-27 20:22:00 -0300

    [OLD REPO] :: uploading a old version of codelab codemoji from 2022

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const elemento1 = document.querySelector('.element-1');
+const elemento2 = document.querySelector('.element-2');
+const elemento3 = document.querySelector('.element-3');
+const elemento4 = document.querySelector('.element-4');
+const elemento5 = document.querySelector('.element-5');
+
+window.addEventListener("load", function(){
+    
+});
